6925fd861c0d281fddffbc237f9175c18e331dfb
victorr <victorr@backbase.com>
2021-09-04 10:46:41 -0500

    :test: definition, define unit tests
    Changes committed:
    	new file:   src/test/java/no/data/CircularBufferTest.java

---
+++ src/test/java/no/data/CircularBufferTest.java
@@ -0,0 +1,26 @@
+package no.data;
+
+import org.junit.Test;
+
+public class CircularBufferTest {
+
+    @Test
+    public void shouldOfferPollableElement(){
+
+    }
+
+    @Test
+    public void shouldNotOfferWhenBufferIsFull(){
+
+    }
+
+    @Test
+    public void shouldNotPollWhenBufferIsEmpty(){
+
+    }
+
+    @Test
+    public void shouldRecycleBuffer(){
+
+    }
+}
