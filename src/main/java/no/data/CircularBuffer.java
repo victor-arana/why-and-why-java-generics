9ae95c338f2e04cef05138e9aaa2e2880009e041
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-11-23 20:16:12 -0600

    :test: compilation, make tests compile
    Changes committed:
    	new file:   src/main/java/no/data/CircularBuffer.java

---
+++ src/main/java/no/data/CircularBuffer.java
@@ -0,0 +1,15 @@
+package no.data;
+
+public class CircularBuffer {
+
+    public CircularBuffer(int i) {
+    }
+
+    public boolean offer(int i) {
+        return false;
+    }
+
+    public int poll() {
+        return 0;
+    }
+}
