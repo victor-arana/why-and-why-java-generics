f2ab2d586bf9772c361f069b4821d04235e96052
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-11-23 20:21:25 -0600

    :test: make tests pass
    Changes committed:
    	modified:   src/main/java/no/data/CircularBuffer.java

--- src/main/java/no/data/CircularBuffer.java
+++ src/main/java/no/data/CircularBuffer.java
@@ -2,14 +2,38 @@ package no.data;
 
 public class CircularBuffer {
 
-    public CircularBuffer(int i) {
+    private final Object[] buffer;
+    private int readCursor = 0;
+    private int writeCursor = 0;
+
+    public CircularBuffer(int size) {
+        buffer = new Object[size];
     }
 
-    public boolean offer(int i) {
-        return false;
+    public boolean offer(int value) {
+        // Check if it's possible to write
+        // a new value
+        if(buffer[writeCursor] != null){
+            return false;
+        }
+
+        buffer[writeCursor] = value;
+        writeCursor = next(writeCursor);
+        return true;
     }
 
-    public int poll() {
-        return 0;
+    public Object poll() {
+        Object value = buffer[readCursor];
+        if( value != null ){
+            buffer[readCursor] = null;
+            readCursor = next(readCursor);
+        }
+        return value;
+    }
+
+    // Returns the next cursor position
+    // in a circular manner
+    private int next(int index) {
+        return (index + 1) % buffer.length;
     }
 }
