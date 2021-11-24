299482717bd20f5be751d6725311ef8c7abd74cc
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-11-23 21:17:05 -0600

    :refactor: refactor methods in order to make them more readable
    Changes committed:
    	modified:   src/main/java/no/data/CircularBuffer.java

--- src/main/java/no/data/CircularBuffer.java
+++ src/main/java/no/data/CircularBuffer.java
@@ -2,38 +2,43 @@ package no.data;
 
 public class CircularBuffer {
 
-    private final Object[] buffer;
+    // Buffer elements
+    private final Object[] elements;
     private int readCursor = 0;
     private int writeCursor = 0;
 
     public CircularBuffer(int size) {
-        buffer = new Object[size];
+        elements = new Object[size];
     }
 
-    public boolean offer(int value) {
+    public boolean offer(Object element) {
         // Check if it's possible to write
-        // a new value
-        if(buffer[writeCursor] != null){
+        // a new element
+        if(elements[writeCursor] == null){
+            elements[writeCursor] = element;
+            writeCursor = next(writeCursor);
+            return true;
+        } else {
             return false;
         }
-
-        buffer[writeCursor] = value;
-        writeCursor = next(writeCursor);
-        return true;
     }
 
     public Object poll() {
-        Object value = buffer[readCursor];
-        if( value != null ){
-            buffer[readCursor] = null;
+        Object element = elements[readCursor];
+        if( element == null ){
+            return null;
+        } else {
+            // remove element from buffer
+            elements[readCursor] = null;
+            // updated readCursor
             readCursor = next(readCursor);
+            return element;
         }
-        return value;
     }
 
     // Returns the next cursor position
     // in a circular manner
     private int next(int index) {
-        return (index + 1) % buffer.length;
+        return (index + 1) % elements.length;
     }
 }
