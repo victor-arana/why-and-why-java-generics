478394d835ed95f6203f3da0068f46008f1aacf9
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-11-28 11:50:52 -0600

    :refactor: docs, add comments to methods
    Changes committed:
    	modified:   src/main/java/no/data/StringCircularBuffer.java

--- src/main/java/no/data/StringCircularBuffer.java
+++ src/main/java/no/data/StringCircularBuffer.java
@@ -1,17 +1,35 @@
 package no.data;
 
+/**
+ * A Circular Buffer also known  as a Ring Buffer is a Queue with a fixed size.
+ * Elements that come in first, come out first.
+ */
 public class StringCircularBuffer {
     public StringCircularBuffer(int bufferSize) {
     }
 
+    /**
+     * Writes a String element to the Buffer.
+     * @return true if succeeds, false otherwise.
+     */
     public boolean offer(String s) {
         return false;
     }
 
+    /**
+     * Reads a String element from the Buffer.
+     * @return The first String element that came into the
+     * buffer or null if the buffer is empty
+     */
     public String poll() {
         return "";
     }
 
+    /**
+     * Print the content of the Buffer
+     * @return A String containing the elements
+     * of the Buffer
+     */
     public String printContent() {
         return "";
     }
