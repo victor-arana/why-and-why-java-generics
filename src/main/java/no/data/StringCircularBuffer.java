4bfaa7b99da1c33e7f3ad69217aee9997157ab1c
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-11-28 11:43:19 -0600

    :test: compile, write code to compile
    Changes committed:
    	new file:   src/main/java/no/data/StringCircularBuffer.java

---
+++ src/main/java/no/data/StringCircularBuffer.java
@@ -0,0 +1,18 @@
+package no.data;
+
+public class StringCircularBuffer {
+    public StringCircularBuffer(int bufferSize) {
+    }
+
+    public boolean offer(String s) {
+        return false;
+    }
+
+    public String poll() {
+        return "";
+    }
+
+    public String printContent() {
+        return "";
+    }
+}
